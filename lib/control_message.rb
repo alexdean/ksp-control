89c5d28a663f1211379d7521234ef3daa4177d02
Alex Dean <github@mostlyalex.com>
2017-11-23 13:28:41 -0600

    more capabilities for ControlMessage

--- lib/control_message.rb
+++ lib/control_message.rb
@@ -1,16 +1,8 @@
 # a message from the control board. a data object.
 class ControlMessage
-  attr_reader :action_group, :throttle, :autopilot_mode
-
   def self.parse(message_str)
-    positions = {
-      action_group: 0,
-      throttle: 1..2,
-      autopilot_mode: 3
-    }
-
     args = {}
-    positions.each do |key, pos|
+    mapping.each do |key, pos|
       val = message_str[pos]
       val = val[0] == '-' ? nil : val.to_i
 
@@ -20,10 +12,77 @@ class ControlMessage
     new(args)
   end
 
-  def initialize(action_group:, throttle:, autopilot_mode:)
-    @action_group = action_group
-    @throttle = throttle
-    @autopilot_mode = autopilot_mode
+  # describes how to map from raw message strings to attribute values
+  #
+  # @return [Hash<Symbol,Integer,Range>]
+  def self.mapping
+    {
+      action_group: 0,
+      throttle: 1..2,
+      autopilot_mode: 3
+    }
+  end
+
+  def self.valid_attrs
+    mapping.keys
+  end
+
+  def initialize(attrs = {})
+    @attrs = attrs
+  end
+
+  # read the value of a given attribute
+  #
+  # raises ArgumentError if an unknown attribute is requested
+  #
+  # @return [Integer,boolean,nil]
+  def read(attr)
+    raise_if_invalid!(attr)
+    @attrs[attr]
+  end
+
+  # TODO: is it right to filter out nils? (how do we send "turn this off?")
+  # nil means "no change". so we don't send those.
+  # "turn this off" is a '0'
+  def read_present
+    @attrs.select { |k,v| !v.nil? }
+  end
+
+  # write a value
+  #
+  # @return the newly set value
+  def write(attr, value)
+    raise_if_invalid!(attr)
+    @attrs[attr] = value
+  end
+
+  # return a new message containing only the fields which differ from
+  # other_message. (all values which match will be nil.)
+  #
+  # @param [ControlMessage] other_message
+  # @return ControlMessage
+  def diff(other_message)
+    out = self.class.new
+
+    self.class.valid_attrs.each do |attr|
+      ours = read(attr)
+      theirs = other_message.read(attr)
+
+      # if action_group has the same value, we should trigger that group again.
+      if ours != theirs || attr == :action_group && !ours.nil?
+        out.write(attr, ours)
+      end
+    end
+
+    out
+  end
+
+  private
+
+  def raise_if_invalid!(attr)
+    if !self.class.valid_attrs.include?(attr)
+      raise ArgumentError, "unknown attribute :#{attr}"
+    end
   end
 end
 
--- spec/lib/control_message_spec.rb
+++ spec/lib/control_message_spec.rb
@@ -7,18 +7,92 @@ RSpec.describe ControlMessage, type: :model do
       input = '0013'
       subject = ControlMessage.parse(input)
 
-      expect(subject.action_group).to eq 0
-      expect(subject.throttle).to eq 1
-      expect(subject.autopilot_mode).to eq 3
+      expect(subject.read(:action_group)).to eq 0
+      expect(subject.read(:throttle)).to eq 1
+      expect(subject.read(:autopilot_mode)).to eq 3
     end
 
     it 'sets nil values' do
       input = '----'
       subject = ControlMessage.parse(input)
 
-      expect(subject.action_group).to eq nil
-      expect(subject.throttle).to eq nil
-      expect(subject.autopilot_mode).to eq nil
+      expect(subject.read(:action_group)).to eq nil
+      expect(subject.read(:throttle)).to eq nil
+      expect(subject.read(:autopilot_mode)).to eq nil
+    end
+  end
+
+  describe '#read' do
+    it 'reads desired attribute' do
+      subject = ControlMessage.new(action_group: 1)
+      expect(subject.read(:action_group)).to eq 1
+    end
+
+    it 'raises exception for unknown attribute' do
+      subject = ControlMessage.new
+      expect {
+        subject.read(:foo)
+      }.to raise_error(ArgumentError, "unknown attribute :foo")
+    end
+  end
+
+  describe '#read_present' do
+    it 'returns hash of non-nil attributes' do
+      subject = ControlMessage.new(action_group: 1, throttle: nil, autopilot_mode: 5)
+      expect(subject.read_present).to(
+        eq(
+          action_group: 1,
+          autopilot_mode: 5
+        )
+      )
+    end
+  end
+
+  describe '#write' do
+    it 'writes an attribute' do
+      subject = ControlMessage.new
+      subject.write(:throttle, 89)
+
+      expect(subject.read(:throttle)).to eq 89
+    end
+
+    it 'raises exception for unknown attribute' do
+      subject = ControlMessage.new
+      expect {
+        subject.write(:foo, 99)
+      }.to raise_error(ArgumentError, "unknown attribute :foo")
+    end
+  end
+
+  describe '#diff' do
+    it 'returns a new message with values that dont match the other message' do
+      older = ControlMessage.new(
+        action_group: nil,
+        throttle: 2,
+        autopilot_mode: 3
+      )
+      newer = ControlMessage.new(
+        action_group: 1,
+        throttle: 2,
+        autopilot_mode: 4
+      )
+
+      out = newer.diff(older)
+      expect(out.read(:action_group)).to eq 1
+      expect(out.read(:throttle)).to eq nil
+      expect(out.read(:autopilot_mode)).to eq 4
+    end
+
+    it 'always returns non-nil action_group' do
+      older = ControlMessage.new(
+        action_group: 1
+      )
+      newer = ControlMessage.new(
+        action_group: 1
+      )
+
+      out = newer.diff(older)
+      expect(out.read(:action_group)).to eq 1
     end
   end
 end
