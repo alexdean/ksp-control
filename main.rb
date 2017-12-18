2430bb7f24209827f6d59bda84c716ac74690533
Alex Dean <github@mostlyalex.com>
2017-12-18 11:25:42 -0600

    cli options for main.rb

--- main.rb
+++ main.rb
@@ -1,25 +1,68 @@
 require 'net/http'
 require 'logger'
 require 'serialport'
+require 'optparse'
 require_relative 'lib/control_state'
 require_relative 'lib/dispatcher'
 require_relative 'lib/timed'
 
 include Timed
 
-serial = SerialPort.new('/dev/tty.usbmodem1441', 9600, 8, 1, SerialPort::NONE)
-
-level = Logger::DEBUG
-
 logger = Logger.new($stdout)
-logger.procname = 'main'
-logger.level = level
+logger.progname = 'main'
 
 dispatch_logger = Logger.new($stdout)
-dispatch_logger.procname = 'dispatch'
-dispatch_logger.level = level
+dispatch_logger.progname = 'dispatch'
 
-dispatcher = Dispatcher.new(logger: dispatch_logger)
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: main.rb [options]"
+
+  opts.on('-v', '--[no-]verbose', 'Run verbosely') do |o|
+    level = o ? Logger::DEBUG : Logger::INFO
+    logger.level = level
+    dispatch_logger.level = level
+  end
+
+  opts.on('-t', '--test-mode', 'Log commands but dont send to telemachus.') do |o|
+    options[:test_mode] = o
+    if o
+      logger.warn "Test mode enabled. No commands will be sent to telemachus."
+    end
+  end
+
+  opts.on(
+    '-d',
+    '--device',
+    'Specify USB connection to use. (Auto-detect is attempted if this is not set.)' \
+      " Example: '--device /dev/tty.usbserial-DN03FAMI'"
+  ) do |o|
+    options[:device] = o
+  end
+end.parse!
+
+def detect_usb_serial_device
+  candidates = Dir['/dev/tty.usb*']
+  candidates.first
+end
+
+if options[:device].nil?
+  detected = detect_usb_serial_device
+  if detected.nil?
+    logger.error "Unable to detect USB device, and no --device specified."
+    logger.error "Exiting."
+    exit 1
+  end
+  options[:device] = detected
+end
+
+logger.info "Connecting to #{options[:device]}"
+
+serial = SerialPort.new(options[:device], 9600, 8, 1, SerialPort::NONE)
+
+dispatcher_options = { logger: dispatch_logger }
+dispatcher_options[:send_commands] = false if options[:test_mode]
+dispatcher = Dispatcher.new(dispatcher_options)
 threads = []
 
 threads << Thread.new {
